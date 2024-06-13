9110c4d498a1489be684772c398c713edb098f18
krishadn <krizziasantillan@gmail.com>
2024-06-13 14:03:27 +0800

    added models

---
+++ model/Course.java
@@ -0,0 +1,19 @@
+public class Course {
+    private int code;
+    private String desc;
+
+    public Course(int code, String desc){
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public int getCode(){
+        return code;
+    }
+
+    public String getDesc(){
+        return desc;
+    }
+
+
+}
---
+++ model/Schedule.java
@@ -0,0 +1,51 @@
+import java.time.LocalDateTime;
+
+public class Schedule{
+    private int id;
+    private int facultyId;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private int courseCode;
+    private int roomId;
+
+
+    public Schedule(int id, int facultyId, LocalDateTime start, LocalDateTime end, int courseCode, int roomId){
+        this.id = id;
+        this.facultyId = facultyId;
+        this.start = start;
+        this.end = end;
+        this.courseCode = courseCode;
+        this.roomId = roomId;
+    }
+
+    
+    public int getId(){
+        return id;
+    }
+
+
+    public int getFacultyId(){
+        return facultyId;
+    }
+
+
+    public LocalDateTime getStart(){
+        return start;
+    }
+
+
+    public LocalDateTime getEnd(){
+        return end;
+    }
+
+
+    public int getCourseCode(){
+        return courseCode;
+    }
+
+
+    public int getRoomId(){
+        return roomId;
+    }
+
+}
