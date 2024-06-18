c9f0c5f44fc4da26b071ed078511e68cc52c0f6b
krishadn <krizziasantillan@gmail.com>
2024-06-18 14:55:15 +0800

    updated controller and queries

--- src/main/java/app/controller/Queries.java
+++ src/main/java/app/controller/Queries.java
@@ -1,22 +1,28 @@
 package app.controller;
 public class Queries {
-  static String createFacultyTable = "CREATE TABLE FACULTY " +
-                      "(ID INTEGER PRIMARY KEY     AUTOINCREMENT," +
-                      " NAME           TEXT    NOT NULL)"; 
+  // static String createFacultyTable = "CREATE TABLE FACULTY " +
+  //                     "(ID INTEGER PRIMARY KEY     AUTOINCREMENT," +
+  //                     " NAME           TEXT    NOT NULL)"; 
 
-  static String createCourseTable = "CREATE TABLE COURSES " +
-                      "(COURSE_CODE TEXT PRIMARY KEY NOT NULL, " +
-                      "COURSE_NAME TEXT NOT NULL)";
+  // static String createCourseTable = "CREATE TABLE COURSES " +
+  //                     "(COURSE_CODE TEXT PRIMARY KEY NOT NULL, " +
+  //                     "COURSE_NAME TEXT NOT NULL)";
 
 
-  static String createScheduleTable = "CREATE TABLE SCHEDULE " +
-                      "(ID INTEGER PRIMARY KEY AUTOINCREMENT, " + 
-                      "";
+  // static String createScheduleTable = "CREATE TABLE SCHEDULE " +
+  //                     "(ID INTEGER PRIMARY KEY AUTOINCREMENT, " + 
+  //                     "";
   
-  public static String insertFaculty(String name) {
-    String query = "INSERT INTO FACULTY ( NAME ) " +
-                      "VALUES ('" + name + "');";
-    return query;
+  public static String insertFaculty(int id, String name) {
+    return String.format("INSERT INTO FACULTY VALUES (%d, '%s');", id, name);
+  }
+  
+  public static String updateFaculty(int id, String name) {
+    return String.format("UPDATE FACULTY SET NAME = '%s' WHERE ID=%d;", name, id);
+  }
+
+  public static String deleteFaculty(int id){
+    return String.format("DELETE FROM FACULTY WHERE ID=%d;", id);
   }
 
   public static String selectAllFrom(String table){
@@ -24,10 +30,7 @@ public class Queries {
     return query;
   }
 
-  public static String deleteFaculty(String name){
-    String query = "DELETE FROM FACULTY WHERE NAME='" + name + "'";
-    return query;
-  }
+  
 
   static String selectFaculty =  "SELECT * FROM FACULTY;";
 
