f3586955275b4cc586020bda4b728e62076461c4
omzkiii <geogeomarsantos@gmail.com>
2024-06-26 12:49:02 +0800

    add CourseView class

--- src/main/java/app/AppController.java
+++ src/main/java/app/AppController.java
@@ -38,8 +38,7 @@ import app.controller.FacultyControllers;
 import app.controller.ScheduleController;
 import app.model.Faculty;
 import app.model.Schedule;
-
-
+import app.view.CourseView;
 import app.controller.CourseControllers;
 import app.model.Course;
 
@@ -55,7 +54,7 @@ public class AppController {
   @FXML
   private BorderPane subPane;
   @FXML
-  private TableView<Course> courseTable;
+  private TableView<Course> courseTbl;
   
   @FXML
   private TableView<Faculty> facultyTbl;
@@ -173,69 +172,13 @@ public class AppController {
       public void run() {
         FxmlLoader object = new FxmlLoader();
         Pane view = object.getPage("course");
-        setCourseCols(view);
+        CourseView.setCourseCols(view, courseTbl);
         subPane.setCenter(view);
       }
     });
   }
 
 
-  public void setCourseCols(Pane view){
-    courseTable = (TableView<Course>) view.lookup("#courseTable");
-    TableColumn<Course, String> codeCol = (TableColumn<Course, String>) courseTable.getColumns().get(0);
-    codeCol.setCellValueFactory(new PropertyValueFactory<>("code"));
-    
-    TableColumn<Course, String> desCol = (TableColumn<Course, String>) courseTable.getColumns().get(1);
-    desCol.setCellValueFactory(new PropertyValueFactory<>("desc"));
-    
-    TableColumn<Course, String> lecUnitsCol = (TableColumn<Course, String>) courseTable.getColumns().get(2);
-    lecUnitsCol.setCellValueFactory(new PropertyValueFactory<>("lecUnits"));
-    
-    TableColumn<Course, String> labUnitsCol = (TableColumn<Course, String>) courseTable.getColumns().get(3);
-    labUnitsCol.setCellValueFactory(new PropertyValueFactory<>("labUnits"));
-
-
-    TableColumn<Course, String> hpweekCol = (TableColumn<Course, String>) courseTable.getColumns().get(4);
-    hpweekCol.setCellValueFactory(new PropertyValueFactory<>("hrsPerWeek"));
-    
-    TableColumn<Course, String> facultyCol = (TableColumn<Course, String>) courseTable.getColumns().get(5);
-    facultyCol.setCellValueFactory(new PropertyValueFactory<>("faculty"));
-
-    TableColumn<Course, Void> actionCol = (TableColumn<Course, Void>) courseTable.getColumns().get(6);
-    actionCol.setCellFactory(setCourseButton());
-
-    for (Course course : CourseControllers.getAllCourse()) {
-      courseTable.getItems().add(course);
-    }
-  }
-
-  private Callback<TableColumn<Course, Void>, TableCell<Course, Void>> setCourseButton() {
-    return new Callback<TableColumn<Course, Void>, TableCell<Course, Void>>() {
-      @Override
-      public TableCell<Course, Void> call(TableColumn<Course, Void> param) {
-        return new TableCell<Course, Void>() {
-          private final Button editButton = new Button("Edit");
-          private final Button deleteButton = new Button("Delete");
-          private final HBox hbox = new HBox(editButton, deleteButton);
-        {
-            editButton.setOnAction(event -> {
-              // TODO: display edit pane
-            });
-            deleteButton.setOnAction(event -> {
-              String course = getTableView().getItems().get(getIndex()).getCode();
-              CourseControllers.removeCourse(course);
-            });
-          }
-
-          @Override
-          protected void updateItem(Void item, boolean empty) {
-            super.updateItem(item, empty);
-            setGraphic(empty ? null : hbox);
-          }
-        };
-      }
-    };
-  }
 
   public void switchToScene2Schedules(ActionEvent event) throws IOException {
     // Load the main scene
